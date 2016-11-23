ee37ba2ae11898284e0318f19b2303463591e574
ccnuyan <ccnuyan@ccnuyans-MacBook-Pro.local>
2016-11-23 08:54:13 +0800

    ready to copy

--- scripts/copyEveryDay.js
+++ scripts/copyEveryDay.js
@@ -1,10 +1,10 @@
 var schedule = require('node-schedule');
 var copy = require('./dateCopy.js');
 
-var j = schedule.scheduleJob('0 0 1 * * *', function () {
-    var today=new Date(); 
-    var t=d.getTime()-1000*60*60*24; 
-    var yesterday=new Date(t);
+var j = schedule.scheduleJob('0 10 1 * * *', function () {
+    var today = new Date();
+    var t = today.getTime() - 1000 * 60 * 60 * 24;
+    var yesterday = new Date(t);
     copy(yesterday);
 });
 
